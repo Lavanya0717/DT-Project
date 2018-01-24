ab1808dc25fe498f92931555b7d9c4034ae56d38
Admin <Admin@DESKTOP-U0HE8RG>
2018-01-24 16:13:07 +0530

    Junit test case implemented

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.Dao.UserDao;
+import com.Model.User;
+
+public class UserController {
+	
+	@Autowired
+	User user;
+	@Autowired
+	UserDao userDao;
+	
+	
+	@RequestMapping("/addUser")
+	public String addUser(@RequestParam Map<String,String> user1,Model m) {
+		
+		user.setEmail(user1.get("email"));
+		user.setAddress(user1.get("address"));
+		user.setCountry(user1.get("country"));
+		user.setGender(user1.get("gender"));
+		user.setName(user1.get("name"));
+		user.setPassword(user1.get("password"));
+		user.setPhone(user1.get("phone"));
+		
+		if(userDao.add(user))
+			return "redirect:login?id=3";
+		return "register";
+	}
+}
--- src/main/java/com/controller/indexController.java
+++ src/main/java/com/controller/indexController.java
@@ -1,16 +1,35 @@
 package com.controller;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 public class indexController {
 
-	 @RequestMapping("/")
-	    public String index() {
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+	
+	
+	
+	
+	
+	
+	/*@Autowired
+	 private UserDao userDao;
+	
+	 @RequestMapping(value={"/","/home","/index"})
+	    public ModelAndView index() {
 	        //mapped to hostname:port/home/index/
-	        return "index";
-	    }
+		 ModelAndView mv=new ModelAndView("index");
+		 mv.addObject("users",userDao.list());		 		 
+	        return mv;
+	    }*/
 }
 
 	
